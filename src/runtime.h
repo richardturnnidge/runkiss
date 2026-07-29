890201279ec21d1f8d6b10535bf5dac8e7aac1de
Richard Turnnidge <117299012+richardturnnidge@users.noreply.github.com>
2026-07-29 21:05:10 +0100

    Add files via upload

--- src/runtime.h
+++ src/runtime.h
@@ -1,9 +1,8 @@
-// #include "text_buffer.h"
+// FUNCTIONS IN RUNTIME.H
 
 void runcode(char* fname);
-//void runcode(text_buffer* aTextBuffer);
 void parseLine(char *command, char *param1, char *param2);
 uint8_t lower(uint8_t num);
-// Removes all occurrences of `sub` from a single string, in place.
 void strip_substr(char *str, const char *sub);
 void strip_substr_array(char *arr[], size_t count, const char *sub);
+void replace_char(char *str, char old_char, char new_char);
